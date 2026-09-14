2b63259792842fb7db2a9430053d82c7ed2e7622
Estefania Bermudez Alvarez <tefyberal09@gmail.com>
2026-09-14 10:47:11 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ GolBet.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GolBet.Entities;
+using GolBet.Services.DTOs;
+namespace GolBet.Services.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Flattening by convention:
+        // MatchDto.HomeTeamName  <- Match.HomeTeam.Name 
+        // MatchDto.AwayTeamCrestUrl <- Match.AwayTeam.CrestUrl 
+        CreateMap<Match, MatchDto>();
+    }
+}
